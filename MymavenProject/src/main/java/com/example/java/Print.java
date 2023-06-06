d5764c5856f48cf7aebfbb3236c16f4857fccd6b
florance <florance@localhost>
2023-06-06 22:57:19 +1000

    second commit

--- MymavenProject/src/main/java/com/example/java/Print.java
+++ MymavenProject/src/main/java/com/example/java/Print.java
@@ -3,7 +3,7 @@ package com.example.java;
 public class Print {
 
 	public static void main(String[] args) {
-		System.out.println(" Java with Maven project");
+		System.out.println(" Java with Maven project ");
 
 	}
 
