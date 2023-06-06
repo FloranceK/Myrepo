7571532bba89265569986da995afbac3b5baf893
smathew <smathew@localhost>
2023-06-06 23:05:41 +1000

    third commmit

--- MymavenProject/src/main/java/com/example/java/Print.java
+++ MymavenProject/src/main/java/com/example/java/Print.java
@@ -3,7 +3,9 @@ package com.example.java;
 public class Print {
 
 	public static void main(String[] args) {
+		//Test message using maven and github
 		System.out.println(" Java with Maven project ");
+		
 
 	}
 
